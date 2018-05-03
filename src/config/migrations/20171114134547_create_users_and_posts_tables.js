2549ff9afa2bef1000e173699bebd9b0fd219efd
martinnord <martin.nordstrom99@gmail.com>
2018-05-03 23:25:42 +0200

    updated migration

--- src/config/migrations/20171114134547_create_users_and_posts_tables.js
+++ src/config/migrations/20171114134547_create_users_and_posts_tables.js
@@ -53,7 +53,7 @@ exports.up = function(knex) {
         .integer('post_id')
         .references('id')
         .inTable('posts')
-        .onDelete()
+        .onDelete('CASCADE')
         .notNullable()
       table.timestamp('created_at').defaultTo(knex.fn.now())
       table.timestamp('updated_at').defaultTo(knex.fn.now())
